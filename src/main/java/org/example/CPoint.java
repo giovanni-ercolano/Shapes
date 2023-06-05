036d1fc86dbe007e4d25a3faf19bc56979bac816
GioErco03 <75883612+giovanni-ercolano@users.noreply.github.com>
2023-06-05 15:24:23 +0200

    Initial commit

---
+++ src/main/java/org/example/CPoint.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Shape extends CPoint {
+
+    private double x;
+    private double y;
+
+    public Shape( double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
---
+++ src/main/java/org/example/Shape.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class CPoint{
+
+    private double Area = 0;
+    private double Perimeter = 0;
+
+    public CPoint() {
+    }
+
+    public double getArea() {
+        return Area;
+    }
+
+    public void setArea(double area) {
+        Area = area;
+    }
+
+    public double getPerimeter() {
+        return Perimeter;
+    }
+
+    public void setPerimeter(double perimeter) {
+        Perimeter = perimeter;
+    }
+}
